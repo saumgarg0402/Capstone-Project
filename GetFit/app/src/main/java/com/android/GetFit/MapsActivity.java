99cbbe52900df2c7551004b191387e210d4b0ec4
saumgarg0402 <saumya.garg0402@mail.com>
2018-03-24 20:19:52 +0530

    Adding complete Tracker

--- GetFit/app/src/main/java/com/android/GetFit/MapsActivity.java
+++ GetFit/app/src/main/java/com/android/GetFit/MapsActivity.java
@@ -17,8 +17,12 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.RoundCap;
 
 import java.io.IOException;
 import java.util.List;
@@ -28,6 +32,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
 
     LocationManager locationManager;
+    private int onLoad;
+    LatLng startLocation;
+    private static final int COLOR_BLACK_ARGB = 0xff000000;
+    private static final int POLYLINE_STROKE_WIDTH_PX = 12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +45,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
+        onLoad=0;
         changeLocation();
 
 
@@ -60,6 +68,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void markStartingLocationOnMap(GoogleMap mapObject,LatLng location,String str){
+        onLoad=1;
+        startLocation=location;
         mapObject.addMarker(new MarkerOptions().position(location).title(str));
         mapObject.moveCamera(CameraUpdateFactory.newLatLngZoom(location,10.2f));
     }
@@ -87,7 +97,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    private void stylePolyline(Polyline polyline) {
+        polyline.setStartCap(new RoundCap());
+        polyline.setEndCap(new RoundCap());
+        polyline.setWidth(POLYLINE_STROKE_WIDTH_PX);
+        polyline.setColor(COLOR_BLACK_ARGB);
+        polyline.setJointType(JointType.ROUND);
+    }
+
     public void changeLocation(){
+
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this, "Please give permission to access location", Toast.LENGTH_SHORT).show();
@@ -105,21 +124,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Toast.makeText(this, "Please enable provider.", Toast.LENGTH_SHORT).show();
             return;
         }
-        locationManager.requestLocationUpdates(provider, 0, 0, new LocationListener() {
+        locationManager.requestLocationUpdates(provider, 60000, 100, new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
                 double latitude=location.getLatitude();
                 double longitude= location.getLongitude();
+
                 LatLng latLng = new LatLng(latitude,longitude);
                 Geocoder geocoder = new Geocoder(getApplicationContext());
-                try {
-                    List<Address> addressList = geocoder.getFromLocation(latitude,longitude,1);
-                    String str = addressList.get(0).getLocality()+","+addressList.get(0).getCountryName();
-                    markStartingLocationOnMap(mMap,latLng,str);
 
-                } catch (IOException e) {
-                    e.printStackTrace();
+                if(onLoad == 0){
+                    try {
+                        List<Address> addressList = geocoder.getFromLocation(latitude,longitude,1);
+                        String str = addressList.get(0).getLocality()+","+addressList.get(0).getCountryName();
+                        markStartingLocationOnMap(mMap,latLng,str);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
+                else{
+                    mMap.clear();
+                    Polyline polyline=mMap.addPolyline(new PolylineOptions().add(startLocation).add(latLng));
+                    stylePolyline(polyline);
+                }
+
             }
 
             @Override
